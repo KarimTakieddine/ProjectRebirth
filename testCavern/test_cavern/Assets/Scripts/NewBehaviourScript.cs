d4ce4ae04583756b227e1e7f760bb8ba0ef756fe
AcidDrippa <arunsomasekh@gmail.com>
2019-01-15 07:41:11 +0000

    COMMIT> Test cavern.  Right now separate project.

---
+++ testCavern/test_cavern/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    Rigidbody rb;
+    public float Speed;
+	// Use this for initialization
+	void Start () {
+        Speed = 0.25f;
+        rb = GetComponent<Rigidbody>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveJump = Input.GetAxis("Jump");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveJump, moveVertical);
+        rb.AddForce(movement * Speed);
+        
+        rb.AddForce(new Vector3(0.0f, 9.8f, 0.0f));
+    }
+}
